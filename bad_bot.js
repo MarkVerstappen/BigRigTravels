2f766a835ad7e11b2389c7a43b4c006488ce4ae8
MarkVerstappen <151457613+MarkVerstappen@users.noreply.github.com>
2023-11-20 19:23:13 +0100

    Add files via upload

---
+++ bad_bot.js
@@ -0,0 +1,10 @@
+/*
+ * Nightbot command:
+ * !commands add -ul=moderator -cd=30 bad $(eval bad_bot('$(query)'); $(urlfetch json https://raw.githubusercontent.com/MarkVerstappen/BigRigTravels/master/bad_bot.js);)
+ */
+function bad_bot(query='') {
+    if (query.trim() === 'bot!') {
+         return 'I will pretend to do better next time 🔧 person.';
+    }
+    return ' ';
+}
