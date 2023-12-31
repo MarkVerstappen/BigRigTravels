eaa04d35a5a06e641785df71d2d8c0ca988c8735
MarkVerstappen <151457613+MarkVerstappen@users.noreply.github.com>
2023-12-31 16:31:24 +0100

    Update amarillo_by.js

--- amarillo_by.js
+++ amarillo_by.js
@@ -2,7 +2,7 @@
  * Nightbot command:
  * !commands add -ul=moderator -cd=30 amarillo $(eval amarillo_by('$(query)'); $(urlfetch json https://raw.githubusercontent.com/MarkVerstappen/BigRigTravels/master/amarillo_by.js);)
  */
-function sing_a(query='') {
+function amarillo_by(query='') {
     if (query.trim() === 'by morning...') {
          return '🎶Up from San Antone... Everything that I got... Is just what I've got on...🎶';
     }
