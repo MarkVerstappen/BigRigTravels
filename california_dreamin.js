0acc7861d838d8fcc5469f73c5132cce0d3e3143
MarkVerstappen <151457613+MarkVerstappen@users.noreply.github.com>
2024-02-03 17:44:20 +0100

    Add files via upload

---
+++ california_dreamin.js
@@ -0,0 +1,10 @@
+/*
+ * Nightbot command:
+ * !commands add -ul=moderator -cd=30 california $(eval california_dreamin('$(query)'); $(urlfetch json https://raw.githubusercontent.com/MarkVerstappen/BigRigTravels/master/california_dreamin.js);)
+ */
+function california_dreamin(query='') {
+    if (query.trim() === 'dreamin...') {
+         return '🎶On such a winter's day...🎶';
+    }
+    return 'California? I'm no topography expert, but lets give another packet to Jemima! 🐸';
+}
